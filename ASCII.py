5f36237859ca120c45d8c636aac72d162c823332
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2020-07-22 19:33:56 +0530

    ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-# pass the image as command line argument
+#pass the image as command line argument
+
 image_path = "sss.jpg"
 img = Image.open(image_path)
 
@@ -10,23 +11,24 @@ new_width = 120
 new_height = aspect_ratio * new_width * 0.35
 img = img.resize((new_width, int(new_height)))
 
-# new size of image
-# print(img.size)
-
 # convert image to greyscale format
 img = img.convert('L')
 
+# getting pixels 
 pixels = img.getdata()
 
 # replace each pixel with a character from array
 chars = ["(",")","#","&","@","$","%","*","\\",":","/"]
-#chars = ["S","H","I","R","L","E","Y","#","$","&","*"]
 
 new_pixels = [chars[pixel//25] for pixel in pixels]
 new_pixels = ''.join(new_pixels)
 
 # split string of chars into multiple strings of length equal to new width and create a list
+
 new_pixels_count = len(new_pixels)
 ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
 ascii_image = "\n".join(ascii_image)
+
+#output on console 
+#note increase console length to see proper output
 print(ascii_image)
